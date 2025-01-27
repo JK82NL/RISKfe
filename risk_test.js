b465bde490fdeb05438629bdcafd49f195fc0662
JK82NL <jk_dance@hotmail.com>
2025-01-27 22:36:15 +0100

    dobbelsteen battle werkend

--- risk_test.js
+++ risk_test.js
@@ -1,10 +1,26 @@
 //global variables
 let player1 = 0;
 let player2 = 0;
+let player1Total = 0;
+let player2Total = 0;
 let dice1 = 0;
 let dice2 = 0;
 let playerTurn = 1;
 
+// Info vastleggen.
+// Game loop die begint op startpunt. p1 dobbelen.
+// P1 roll
+// P2 roll
+// Als die bekend zijn winnaar calculeren.
+
+// Maak pure functies en alleen voor de andere zaken de side effects (functions). Pure functies.
+// Classes checken. kan zowel data modellen als functies opslaan.
+// Class is eigenlijk een blauwdruk om een object van die class te maken.
+// Denk hierbij aan een recept.
+
+// invoerfunctie,
+//     game functie,
+//         roll functie.
 
 function player1Army() {
     player1 = document.getElementById("attackerInput").value;
@@ -20,33 +36,54 @@ function player2Army() {
 }
 
 function diceRoll() {
+    winnerCalculation()
     dice1 = Math.floor(Math.random() * 6) + 1;
     dice2 = Math.floor(Math.random() * 6) + 1;
     document.getElementById("dice1").innerHTML = dice1;
     document.getElementById("dice2").innerHTML = dice2;
     console.log(dice1, dice2);
 
-    document.getElementById(`player${playerTurn}Points1`).innerHTML = dice1;
-    document.getElementById(`player${playerTurn}Points2`).innerHTML = dice2;
-
     if (playerTurn == 1) {
+        player1Total = dice1 + dice2;
+        document.getElementById(`player${playerTurn}Points1`).innerHTML = dice1;
+        document.getElementById(`player${playerTurn}Points2`).innerHTML = dice2;
         playerTurn = 2;
-        let player1Total = dice1 + dice2;
+        console.log("Player 1 rolled:", player1Total);
     } else if (playerTurn == 2) {
+        player2Total = dice1 + dice2;
+        document.getElementById(`player${playerTurn}Points1`).innerHTML = dice1;
+        document.getElementById(`player${playerTurn}Points2`).innerHTML = dice2;
+        playerTurn = 3;
+        console.log("Player 2 rolled:", player2Total);
+    } else {
+        manDown(player1Total, player2Total);
         playerTurn = 1;
-        let player2Total = dice1 + dice2;
-        winnerCalculation();
-        return player1Total, player2Total;
+        player1Total = 0;
+        player2Total = 0;
     }
+}
 
-    function winnerCalculation(player1Total, player2Total) {
-        if (player1Total > player2Total) {
-            player1--;
-            document.getElementById("player1Army").value = player1;
-        } else if (player2Total > player1Total) {
-            player2--;
-            document.getElementById("player2Army").value = player2;
-        }
+function manDown(player1Total, player2Total) {
+    if (player1Total > player2Total) {
+        player2--;
+        document.getElementById("player2Army").innerHTML = player2;
+        document.getElementById("winMessage").innerHTML = "Defender lost a soldier!";
+    } else if (player2Total > player1Total) {
+        player1--;
+        document.getElementById("player1Army").innerHTML = player1;
+        document.getElementById("winMessage").innerHTML = "Attacker lost a soldier!";
+    } else if (player1Total == player2Total) {
+        document.getElementById("winMessage").innerHTML = "Draw! Meaning the attacker loses";
+    } else {
+        document.getElementById("winMessage").innerHTML = "Error";
+    }
+}
+
+function winnerCalculation() {
+    if (player1 == 0) {
+        document.getElementById("winMessage").innerHTML = "The defender wins the battle!";
+    } else if (player2 == 0) {
+        document.getElementById("winMessage").innerHTML = "The attacker wins the battle!";
     }
 }
 
@@ -66,4 +103,3 @@ function diceRoll() {
 // } else if (dice2 > dice1) {
 //     player1--;
 //     document.getElementById("player2Army").value = player1;
-// }
