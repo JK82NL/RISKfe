aeaed679ffc9cf8e376ba29b111e16f119f97dcf
JK82NL <jk_dance@hotmail.com>
2025-01-28 12:13:55 +0100

    The base for dice battle is finished

--- riskMap.js
+++ riskMap.js
@@ -1,4 +1,50 @@
-let gebieden = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let gebieden = [
+    [0, 0, 0, 0]
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]; // Model/Masked Array
+
+let player1 = {
+    name: "player1", // Ombouwen naar userinput.
+    turn: 0,
+    soldiers: 24,
+    areasOccupied: [0, 0, 0, 0],
+}
+
+let player2 = {
+    name: "player2", // Ombouwen naar userinput.
+    turn: 0,
+    soldiers: 24,
+    areasOccupied: [0, 0, 0, 0],
+}
+
+let player3 = {
+    name: "player3", // Ombouwen naar userinput.
+    turn: 0,
+    soldiers: 24,
+    areasOccupied: [0, 0, 0, 0],
+}
+
+function playerTurns() {
+    let gameRound = 0
+
+    // Bij elke case wordt gecheckt welke ronde elke persoon heeft. daarna plus 1
+    switch (ronde) {
+        case player1.turn:
+            console.log("Player 1's Turn");
+            player1.turn = player1.turn + 1;
+            break;
+        case player2.turn:
+            console.log("Player 2's Turn");
+            player2.turn = player2.turn + 1;
+            break;
+        case player3.turn:
+            console.log("Player 3's Turn");
+            player3.turn = player3.turn + 1;
+            break;
+    }
+}
 // OP ELK GEBIED KUNNEN MEER STATES GEZET WORDEN VOOR DE VEROVERING EN OOK BIJV AANTAL TROEPEN. DIT KAN OOK IN EEN ANDERE MASKED ARRAY.
 
 
@@ -17,4 +63,3 @@ function aangrenzendeGebiedenCheck() {
         }
     }
 }
-// -1 +1 -4 +4
